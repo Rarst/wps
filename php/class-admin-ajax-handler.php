1c098fa26f35412a17e2c32bae5d69111d7362f2
Andrey Savchenko <contact@rarst.net>
2014-10-11 22:19:39 +0300

    Implemented WP-specific ajax handler class.

---
+++ php/class-admin-ajax-handler.php
@@ -0,0 +1,44 @@
+<?php
+namespace Rarst\wps;
+
+use Whoops\Exception\Formatter;
+use Whoops\Handler\Handler;
+use Whoops\Handler\JsonResponseHandler;
+use Whoops\Util\Misc;
+
+/**
+ * WordPress-specific version of Json handler.
+ */
+class Admin_Ajax_Handler extends JsonResponseHandler {
+
+	/**
+	 * @return bool
+	 */
+	private function isAjaxRequest() {
+
+		return defined( 'DOING_AJAX' ) && DOING_AJAX;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function handle() {
+
+		if ( $this->onlyForAjaxRequests() && ! $this->isAjaxRequest() ) {
+			return Handler::DONE;
+		}
+
+		$response = array(
+			'success' => false,
+			'data'    => Formatter::formatExceptionAsDataArray( $this->getInspector(), $this->addTraceToOutput() ),
+		);
+
+		if ( Misc::canSendHeaders() ) {
+			header( 'Content-Type: application/json; charset=' . get_option( 'blog_charset' ) );
+		}
+
+		echo json_encode( $response );
+
+		return Handler::QUIT;
+	}
+} 
