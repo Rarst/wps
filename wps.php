285bd9bbfbca84477cd9a26faf0e2963199833f0
Andrey Savchenko <contact@rarst.net>
2015-06-11 12:23:24 +0300

    Added a way to disable for current load with wps_disable in GET query

--- wps.php
+++ wps.php
@@ -31,5 +31,9 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 }
 
+if ( isset( $_GET['wps_disable'] ) ) {
+	return;
+}
+
 $wps = new \Rarst\wps\Plugin();
 $wps->run();
